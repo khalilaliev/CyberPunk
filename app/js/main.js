9c303a7bb2094a8b4d43f3223e1eacb680ae3655
Khalil <khalilaliev0@gmail.com>
2024-01-15 05:26:45 +0100

    cyberpunk layout

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.slider__swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3500,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    }
+  });
